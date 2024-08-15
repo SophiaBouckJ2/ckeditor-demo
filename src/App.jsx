764771b1ce358e99b93698b1e2f47778eabb0119
SophiaBouckJ2 <sophiab@j2consultants.com>
2024-08-15 11:45:02 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,355 @@
+import { useState, useEffect, useRef } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+import {
+  DecoupledEditor,
+  AccessibilityHelp,
+  Alignment,
+  Autosave,
+  BalloonToolbar,
+  CloudServices,
+  Essentials,
+  GeneralHtmlSupport,
+  Heading,
+  HtmlEmbed,
+  Indent,
+  IndentBlock,
+  List,
+  ListProperties,
+  Paragraph,
+  PasteFromOffice,
+  SelectAll,
+  SpecialCharacters,
+  Style,
+  Undo,
+} from "ckeditor5";
+import {
+  ExportPdf,
+  ExportWord,
+  ImportWord,
+  MultiLevelList,
+} from "ckeditor5-premium-features";
+
+import "ckeditor5/ckeditor5.css";
+import "ckeditor5-premium-features/ckeditor5-premium-features.css";
+
+import "./App.css";
+
+/**
+ * Please update the following values with your actual tokens.
+ * Instructions on how to obtain them: https://ckeditor.com/docs/trial/latest/guides/real-time/quick-start.html
+ */
+const LICENSE_KEY =
+  "TFAvdUJ3b0ZnQStZbXdNb2NZMUZVRDM2bzZ4MjZLQmN5U1NscGN0MUUxL25zRFgzY1lEQjFxQTNoTldlQ1E9PS1NakF5TkRBNU1UUT0=";
+
+export default function App() {
+  const editorContainerRef = useRef(null);
+  const editorMenuBarRef = useRef(null);
+  const editorToolbarRef = useRef(null);
+  const editorRef = useRef(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  const editorConfig = {
+    toolbar: {
+      items: [
+        "undo",
+        "redo",
+        "|",
+        "heading",
+        "style",
+        "|",
+        "alignment",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "multiLevelList",
+        "outdent",
+        "indent",
+      ],
+      shouldNotGroupWhenFull: false,
+    },
+    plugins: [
+      AccessibilityHelp,
+      Alignment,
+      Autosave,
+      BalloonToolbar,
+      CloudServices,
+      Essentials,
+      ExportPdf,
+      ExportWord,
+      GeneralHtmlSupport,
+      Heading,
+      HtmlEmbed,
+      ImportWord,
+      Indent,
+      IndentBlock,
+      List,
+      ListProperties,
+      MultiLevelList,
+      Paragraph,
+      PasteFromOffice,
+      SelectAll,
+      SpecialCharacters,
+      Style,
+      Undo,
+    ],
+    balloonToolbar: ["bulletedList", "numberedList"],
+    exportPdf: {
+      stylesheets: [
+        /* This path should point to application stylesheets. */
+        /* See: https://ckeditor.com/docs/ckeditor5/latest/features/converters/export-pdf.html */
+        "./App.css",
+        /* Export PDF needs access to stylesheets that style the content. */
+        "https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.css",
+        "https://cdn.ckeditor.com/ckeditor5-premium-features/43.0.0/ckeditor5-premium-features.css",
+      ],
+      fileName: "export-pdf-demo.pdf",
+      converterOptions: {
+        format: "A4",
+        margin_top: "20mm",
+        margin_bottom: "20mm",
+        margin_right: "12mm",
+        margin_left: "12mm",
+        page_orientation: "portrait",
+      },
+    },
+    exportWord: {
+      stylesheets: [
+        /* This path should point to application stylesheets. */
+        /* See: https://ckeditor.com/docs/ckeditor5/latest/features/converters/export-word.html */
+        "./App.css",
+        /* Export Word needs access to stylesheets that style the content. */
+        "https://cdn.ckeditor.com/ckeditor5/43.0.0/ckeditor5.css",
+        "https://cdn.ckeditor.com/ckeditor5-premium-features/43.0.0/ckeditor5-premium-features.css",
+      ],
+      fileName: "export-word-demo.docx",
+      converterOptions: {
+        document: {
+          orientation: "portrait",
+          size: "A4",
+          margins: {
+            top: "20mm",
+            bottom: "20mm",
+            right: "12mm",
+            left: "12mm",
+          },
+        },
+      },
+    },
+    heading: {
+      options: [
+        {
+          model: "paragraph",
+          title: "Paragraph",
+          class: "ck-heading_paragraph",
+        },
+        {
+          model: "heading1",
+          view: "h1",
+          title: "Heading 1",
+          class: "ck-heading_heading1",
+        },
+        {
+          model: "heading2",
+          view: "h2",
+          title: "Heading 2",
+          class: "ck-heading_heading2",
+        },
+        {
+          model: "heading3",
+          view: "h3",
+          title: "Heading 3",
+          class: "ck-heading_heading3",
+        },
+        {
+          model: "heading4",
+          view: "h4",
+          title: "Heading 4",
+          class: "ck-heading_heading4",
+        },
+        {
+          model: "heading5",
+          view: "h5",
+          title: "Heading 5",
+          class: "ck-heading_heading5",
+        },
+        {
+          model: "heading6",
+          view: "h6",
+          title: "Heading 6",
+          class: "ck-heading_heading6",
+        },
+      ],
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /^.*$/,
+          styles: true,
+          attributes: true,
+          classes: true,
+        },
+      ],
+    },
+    initialData:
+      "<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou've successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What's next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor's\n\t\tconfiguration to match your application's style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don't hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<p>\n\t<i>An editor without the </i><code>Link</code>\n\t<i>plugin? That's brave! We hope the links below will be useful anyway </i\n\t>😉\n</p>\n<ul>\n\t<li>\n\t\t📝 Trial sign up: https://orders.ckeditor.com/trial/premium-features,\n\t</li>\n\t<li>\n\t\t📕 Documentation:\n\t\thttps://ckeditor.com/docs/ckeditor5/latest/installation/index.html,\n\t</li>\n\t<li>\n\t\t⭐️ GitHub (star us if you can!): https://github.com/ckeditor/ckeditor5,\n\t</li>\n\t<li>🏠 CKEditor Homepage: https://ckeditor.com,</li>\n\t<li>🧑‍💻 CKEditor 5 Demos: https://ckeditor.com/ckeditor-5/demo/</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser's\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n",
+    licenseKey: LICENSE_KEY,
+    list: {
+      properties: {
+        styles: true,
+        startIndex: true,
+        reversed: true,
+      },
+    },
+    menuBar: {
+      isVisible: true,
+    },
+    placeholder: "Type or paste your content here!",
+    style: {
+      definitions: [
+        {
+          name: "Article category",
+          element: "h3",
+          classes: ["category"],
+        },
+        {
+          name: "Title",
+          element: "h2",
+          classes: ["document-title"],
+        },
+        {
+          name: "Subtitle",
+          element: "h3",
+          classes: ["document-subtitle"],
+        },
+        {
+          name: "Info box",
+          element: "p",
+          classes: ["info-box"],
+        },
+        {
+          name: "Side quote",
+          element: "blockquote",
+          classes: ["side-quote"],
+        },
+        {
+          name: "Marker",
+          element: "span",
+          classes: ["marker"],
+        },
+        {
+          name: "Spoiler",
+          element: "span",
+          classes: ["spoiler"],
+        },
+        {
+          name: "Code (dark)",
+          element: "pre",
+          classes: ["fancy-code", "fancy-code-dark"],
+        },
+        {
+          name: "Code (bright)",
+          element: "pre",
+          classes: ["fancy-code", "fancy-code-bright"],
+        },
+      ],
+    },
+  };
+
+  configUpdateAlert(editorConfig);
+
+  return (
+    <div>
+      <div className="main-container">
+        <div
+          className="editor-container editor-container_document-editor editor-container_include-style"
+          ref={editorContainerRef}
+        >
+          <div
+            className="editor-container__menu-bar"
+            ref={editorMenuBarRef}
+          ></div>
+          <div
+            className="editor-container__toolbar"
+            ref={editorToolbarRef}
+          ></div>
+          <div className="editor-container__editor-wrapper">
+            <div className="editor-container__editor">
+              <div ref={editorRef}>
+                {isLayoutReady && (
+                  <CKEditor
+                    onReady={(editor) => {
+                      editorToolbarRef.current.appendChild(
+                        editor.ui.view.toolbar.element
+                      );
+                      editorMenuBarRef.current.appendChild(
+                        editor.ui.view.menuBarView.element
+                      );
+                    }}
+                    onAfterDestroy={() => {
+                      Array.from(editorToolbarRef.current.children).forEach(
+                        (child) => child.remove()
+                      );
+                      Array.from(editorMenuBarRef.current.children).forEach(
+                        (child) => child.remove()
+                      );
+                    }}
+                    editor={DecoupledEditor}
+                    config={editorConfig}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * This function exists to remind you to update the config needed for premium features.
+ * The function can be safely removed. Make sure to also remove call to this function when doing so.
+ */
+function configUpdateAlert(config) {
+  if (configUpdateAlert.configUpdateAlertShown) {
+    return;
+  }
+
+  const isModifiedByUser = (currentValue, forbiddenValue) => {
+    if (currentValue === forbiddenValue) {
+      return false;
+    }
+
+    if (currentValue === undefined) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const valuesToUpdate = [];
+
+  configUpdateAlert.configUpdateAlertShown = true;
+
+  if (!isModifiedByUser(config.licenseKey, "<YOUR_LICENSE_KEY>")) {
+    valuesToUpdate.push("LICENSE_KEY");
+  }
+
+  if (valuesToUpdate.length) {
+    window.alert(
+      [
+        "Please update the following values in your editor config",
+        "in order to receive full access to the Premium Features:",
+        "",
+        ...valuesToUpdate.map((value) => ` - ${value}`),
+      ].join("\n")
+    );
+  }
+}
